4c93efb14726477b550811e239d878078b7900e0
Diego Magdaleno <dmmagdal@ucsc.edu>
2020-11-16 11:02:01 -0800

    Created the py file for the text downloader.

---
+++ download_text.py
@@ -0,0 +1,18 @@
+# download_text.py
+# author: Diego Magdaleno
+# This program scrapes various works of fiction from sources such as Project Gutenburg,
+# Open Library, Archive of our Own (Ao3), and Fan Fiction.net (FFnet). These texts are
+# saved locally in a compressed format, but are uncompressed into txt files for
+# training OpenAI's GPT-2 model.
+# Python 3
+# Windows/MacOS/Linux
+
+
+import os
+import json
+import requests
+from bs4 import BeautifulSoup as bsoup
+
+
+def main():
+    pass
